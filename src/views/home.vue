5104a759a217ec62f8c8001fd1eee654bf1a84af
lhw5072061.00 <lihongwei0305>
2022-05-27 14:07:53 +0800

    init

---
+++ src/views/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <h3>home</h3>
+  <button @click="toDetail">to Detail</button>
+</template>
+
+<script>
+export default {
+  name: "home"
+}
+</script>
+<script setup>
+const toDetail = () => {
+
+}
+</script>
+
+<style scoped>
+
+</style>
